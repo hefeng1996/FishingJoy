19ec574bdab8a39c6c3573f763fc3d8a7b9fbab2
hefeng1996 <32031468+hefeng1996@users.noreply.github.com>
2017-12-11 23:54:36 +0800

    Add files via upload
    实现鱼的游动（未实现金币显示）

--- FishLayer.cpp
+++ FishLayer.cpp
@@ -1,5 +1,11 @@
 #include "FishLayer.h"
 #include "Fish.h"
+//#include <ctime>
+enum{
+	k_Direction_Left =0,
+	k_Direction_Right,
+	k_Direction_Count
+};
 
 FishLayer::FishLayer(void)
 {
@@ -15,12 +21,12 @@ bool FishLayer::init()
 		}
 		_fishes = CCArray::createWithCapacity(FISH_MAX_COUNT);
 		CC_SAFE_RETAIN(_fishes);
-		srand(time(0));
 		for(int i = 0; i < FISH_MAX_COUNT; i++){
-			int type = (rand()%100/(double)101) * k_Fish_Type_Count - 1;
+			int type = CCRANDOM_0_1() * k_Fish_Type_Count - 1;
 			Fish* fish = Fish::create((FishType)type);
 			_fishes->addObject(fish);
 		}
+		srand((unsigned)time(0));
 		this->schedule(schedule_selector(FishLayer::addFish), 3.0f);
 		return true;
 	} while (0);
@@ -29,9 +35,8 @@ bool FishLayer::init()
 
 void FishLayer::addFish(float delta)
 	{
-		int addToCount = (rand()%100/(double)101) * 5 + 1;
+		int addToCount = CCRANDOM_0_1() * 5 + 1;
 		int count = 0;
-		 srand(time(0));
 		CCObject* obj;
 		CCSize winSize = CCDirector::sharedDirector()->getWinSize();
 		CCARRAY_FOREACH(_fishes, obj)
@@ -41,9 +46,11 @@ void FishLayer::addFish(float delta)
 			{
 				continue;
 			}
-			this->addChild(fish);
-			fish->setPosition(ccp((rand()%100/(double)101)* winSize.width, (rand()%100/(double)101) * winSize.height));
+			//this->addChild(fish);
+			//fish->setPosition(ccp(CCRANDOM_0_1() * winSize.width, CCRANDOM_0_1() * winSize.height));
+			
 			count++;
+			resetFish(fish);
 			if(count == addToCount)
 			{
 				break;
@@ -53,4 +60,37 @@ void FishLayer::addFish(float delta)
 
 FishLayer::~FishLayer(void)
 {
+}
+
+CCArray* FishLayer::getFishArray()
+{
+	return _fishes;
+}
+
+void FishLayer::resetFish(Fish* fish)
+{
+	int direction = CCRANDOM_0_1() * k_Direction_Count;
+	float startX,startY,endX,endY;
+	CCSize winSize =CCDirector::sharedDirector()->getWinSize();
+	CCSize fishSize =fish->getSize();
+	if(direction==k_Direction_Left)
+	{
+		startX=winSize.width+fishSize.width/2;
+		fish->setRotation(0);
+		endX=-fishSize.width/2;
+	}
+	else
+	{
+		endX=winSize.width+fishSize.width/2;
+		fish->setRotation(180);
+		startX=-fishSize.width/2;
+
+		
+	}
+	endY =CCRANDOM_0_1() *(winSize.height-fishSize.height)+fishSize.height/2;
+	startY=CCRANDOM_0_1() *(winSize.height-fishSize.height)+fishSize.height /2;
+	addChild(fish);
+	fish->setPosition(ccp(startX,startY));
+	fish->moveTo(ccp(endX,endY));
+
 }
